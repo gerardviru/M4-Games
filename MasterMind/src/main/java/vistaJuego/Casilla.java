d30cc3f9e68e5b4f1e4e101bb0eccdd987775a06
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-05-05 12:51:56 +0200

    clases casilla

---
+++ MasterMind/src/main/java/vistaJuego/Casilla.java
@@ -0,0 +1,13 @@
+package vistaJuego;
+
+public class Casilla {
+	
+	private int posicion;
+	private String color;
+	
+	public Casilla(int posicion, String color) {
+		this.posicion = posicion;
+		this.color = color;
+	}
+
+}
---
+++ MasterMind/src/main/java/vistaJuego/Controlador.java
@@ -0,0 +1,9 @@
+package vistaJuego;
+
+public class Controlador {
+
+	private String[] colores = {"magenta", "cian", "amarillo", "verde"};
+	
+	
+	
+}
