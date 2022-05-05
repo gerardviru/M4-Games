aac4e1d3cce8c401a89aceadae8753ac1be31784
Gerard Viñuela <gerardviru@gmail.com>
2022-05-05 09:33:13 +0200

    Inicio proyecto MasterMind

---
+++ MasterMind/src/main/java/masterMind_Games/MasterMind/mastermindApp.java
@@ -0,0 +1,13 @@
+package masterMind_Games.MasterMind;
+
+/**
+ * Hello world!
+ *
+ */
+public class mastermindApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
