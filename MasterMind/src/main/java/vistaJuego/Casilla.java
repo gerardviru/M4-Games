16db0436a05218909ae1053dabaded41980e80f6
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-05-05 13:33:46 +0200

    update casilla controlador

--- MasterMind/src/main/java/vistaJuego/Casilla.java
+++ MasterMind/src/main/java/vistaJuego/Casilla.java
@@ -10,4 +10,34 @@ public class Casilla {
 		this.color = color;
 	}
 
+	/**
+	 * @return the posicion
+	 */
+	public int getPosicion() {
+		return posicion;
+	}
+
+	/**
+	 * @param posicion the posicion to set
+	 */
+	public void setPosicion(int posicion) {
+		this.posicion = posicion;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+
 }
--- MasterMind/src/main/java/vistaJuego/Controlador.java
+++ MasterMind/src/main/java/vistaJuego/Controlador.java
@@ -1,9 +1,31 @@
 package vistaJuego;
 
+import java.util.Iterator;
+
 public class Controlador {
 
-	private String[] colores = {"magenta", "cian", "amarillo", "verde"};
-	
-	
-	
+	private String[] colores = { "magenta", "cian", "amarillo", "verde" };
+
+	public Controlador() {
+
+	}
+
+	public void cambiarColor(int mouseButton) {
+//		Casilla.setColor(colores[x]);
+		int posicion = 0;
+		
+		switch (mouseButton) {
+		case 1:
+			
+			break;
+		case 3:
+			
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
 }
