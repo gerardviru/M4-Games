8f3b4a0393948000b39124ada9a9a13163ff4567
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-05-06 08:59:46 +0200

    modificiacion vistajuego movimiento del contenedor

--- MasterMind/src/main/java/vistaJuego/VistaJuego.java
+++ MasterMind/src/main/java/vistaJuego/VistaJuego.java
@@ -12,6 +12,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.GridLayout;
 
 public class VistaJuego extends JFrame {
 
@@ -50,55 +51,67 @@ public class VistaJuego extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(658, 106, 186, 74);
-		panel.setOpaque(false);
-		contentPane.add(panel);
-		panel.setLayout(null);
+		JPanel coloresDisponiblesPanel = new JPanel();
+		coloresDisponiblesPanel.setBounds(639, 81, 235, 74);
+		coloresDisponiblesPanel.setOpaque(false);
+		contentPane.add(coloresDisponiblesPanel);
+		coloresDisponiblesPanel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Colores Disponibles");
 		lblNewLabel.setBounds(10, 11, 166, 14);
-		panel.add(lblNewLabel);
+		coloresDisponiblesPanel.add(lblNewLabel);
 		
 		JEditorPane editorPane = new JEditorPane();
 		editorPane.setEditable(false);
 		editorPane.setBackground(Color.MAGENTA);
 		editorPane.setBounds(10, 36, 21, 20);
-		panel.add(editorPane);
+		coloresDisponiblesPanel.add(editorPane);
 		
 		JEditorPane editorPane_1 = new JEditorPane();
 		editorPane_1.setEditable(false);
 		editorPane_1.setBackground(Color.YELLOW);
-		editorPane_1.setBounds(57, 36, 21, 20);
-		panel.add(editorPane_1);
+		editorPane_1.setBounds(56, 36, 21, 20);
+		coloresDisponiblesPanel.add(editorPane_1);
 		
 		JEditorPane editorPane_2 = new JEditorPane();
 		editorPane_2.setEditable(false);
 		editorPane_2.setBackground(Color.CYAN);
-		editorPane_2.setBounds(107, 36, 21, 20);
-		panel.add(editorPane_2);
+		editorPane_2.setBounds(97, 36, 21, 20);
+		coloresDisponiblesPanel.add(editorPane_2);
 		
 		JEditorPane editorPane_3 = new JEditorPane();
 		editorPane_3.setEditable(false);
 		editorPane_3.setBackground(Color.GREEN);
-		editorPane_3.setBounds(155, 36, 21, 20);
-		panel.add(editorPane_3);
+		editorPane_3.setBounds(139, 36, 21, 20);
+		coloresDisponiblesPanel.add(editorPane_3);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setLayout(null);
-		panel_1.setOpaque(false);
-		panel_1.setBounds(658, 248, 186, 74);
-		contentPane.add(panel_1);
+		JEditorPane editorPane_3_1 = new JEditorPane();
+		editorPane_3_1.setEditable(false);
+		editorPane_3_1.setBackground(Color.ORANGE);
+		editorPane_3_1.setBounds(175, 36, 21, 20);
+		coloresDisponiblesPanel.add(editorPane_3_1);
+		
+		JEditorPane editorPane_3_2 = new JEditorPane();
+		editorPane_3_2.setEditable(false);
+		editorPane_3_2.setBackground(Color.BLUE);
+		editorPane_3_2.setBounds(204, 36, 21, 20);
+		coloresDisponiblesPanel.add(editorPane_3_2);
+		
+		JPanel cambinacionPanel = new JPanel();
+		cambinacionPanel.setLayout(null);
+		cambinacionPanel.setOpaque(false);
+		cambinacionPanel.setBounds(658, 248, 186, 74);
+		contentPane.add(cambinacionPanel);
 		
 		JLabel lblCombinacinSecreta = new JLabel("Combinación Secreta");
 		lblCombinacinSecreta.setBounds(10, 11, 166, 14);
-		panel_1.add(lblCombinacinSecreta);
+		cambinacionPanel.add(lblCombinacinSecreta);
 		
-		JPanel panel_1_1 = new JPanel();
-		panel_1_1.setLayout(null);
-		panel_1_1.setOpaque(false);
-		panel_1_1.setBounds(20, 47, 425, 74);
-		contentPane.add(panel_1_1);
+		JPanel insertarPanel = new JPanel();
+		insertarPanel.setLayout(null);
+		insertarPanel.setOpaque(false);
+		insertarPanel.setBounds(20, 47, 425, 74);
+		contentPane.add(insertarPanel);
 		
 		JEditorPane editorPane_4 = new JEditorPane();
 		editorPane_4.setEditable(false);
@@ -135,12 +148,12 @@ public class VistaJuego extends JFrame {
 			}
 		});
 		editorPane_4.setBounds(35, 30, 21, 20);
-		panel_1_1.add(editorPane_4);
+		insertarPanel.add(editorPane_4);
 		
 		
 		JEditorPane editorPane_5 = new JEditorPane();
 		editorPane_5.setBounds(98, 30, 21, 20);
-		panel_1_1.add(editorPane_5);
+		insertarPanel.add(editorPane_5);
 		editorPane_5.setEditable(false);
 		editorPane_5.setBackground(Color.WHITE);
 		editorPane_5.addMouseListener(new MouseListener() {
@@ -175,7 +188,7 @@ public class VistaJuego extends JFrame {
 		
 		JEditorPane editorPane_6 = new JEditorPane();
 		editorPane_6.setBounds(167, 30, 21, 20);
-		panel_1_1.add(editorPane_6);
+		insertarPanel.add(editorPane_6);
 		editorPane_6.setEditable(false);
 		editorPane_6.setBackground(Color.WHITE);
 		editorPane_6.addMouseListener(new MouseListener() {
@@ -239,35 +252,45 @@ public class VistaJuego extends JFrame {
 			}
 		});
 		editorPane_7.setBounds(231, 30, 21, 20);
-		panel_1_1.add(editorPane_7);
+		insertarPanel.add(editorPane_7);
 		
-		JButton btnNewButton = new JButton("Comprobar");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton comprobarBtn = new JButton("Comprobar");
+		comprobarBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				controlador.validacion(editorPane_4.getBackground(),controlador.getColoresSecretos().get(0));
 				controlador.validacion(editorPane_5.getBackground(),controlador.getColoresSecretos().get(1));
 				controlador.validacion(editorPane_6.getBackground(),controlador.getColoresSecretos().get(2));
 				controlador.validacion(editorPane_7.getBackground(),controlador.getColoresSecretos().get(3));
+				
+				// Mover el insertPanel
+				insertarPanel.setBounds(200, 200, 300, 350);
+				contentPane.revalidate();
+				
 			}
 		});
-		btnNewButton.setBounds(287, 30, 116, 23);
-		panel_1_1.add(btnNewButton);
+		comprobarBtn.setBounds(287, 30, 116, 23);
+		insertarPanel.add(comprobarBtn);
+		
+		JPanel validados = new JPanel();
+		validados.setBounds(455, 81, 174, 24);
+		contentPane.add(validados);
+		validados.setLayout(null);
 		
 		JEditorPane verificado1 = new JEditorPane();
-		verificado1.setBounds(451, 160, 26, 20);
-		contentPane.add(verificado1);
+		verificado1.setBounds(1, 0, 32, 24);
+		validados.add(verificado1);
 		
 		JEditorPane verificado2 = new JEditorPane();
-		verificado2.setBounds(486, 160, 26, 20);
-		contentPane.add(verificado2);
+		verificado2.setBounds(49, 0, 32, 24);
+		validados.add(verificado2);
 		
 		JEditorPane verificado3 = new JEditorPane();
-		verificado3.setBounds(527, 160, 26, 20);
-		contentPane.add(verificado3);
+		verificado3.setBounds(97, 0, 32, 24);
+		validados.add(verificado3);
 		
 		JEditorPane verificado4 = new JEditorPane();
-		verificado4.setBounds(563, 160, 26, 20);
-		contentPane.add(verificado4);
+		verificado4.setBounds(145, 0, 29, 24);
+		validados.add(verificado4);
 		
 	}
 }
